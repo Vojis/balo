ddafee0cbf9bee5ef5488ea114a605cdc8cd5f79
voja <voja.milenkovic@nfq.asia>
2020-05-29 14:20:33 +0200

    Create pairs

--- client/src/components/CollectionPairs.js
+++ client/src/components/CollectionPairs.js
@@ -1,23 +1,113 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import PeopleIcon from '@material-ui/icons/People';
+import PairDialog from './dialogs/PairDialog';
 
 const useStyles = makeStyles((theme) => ({
   collectionName: {
     color: '#fff',
-  }
+  },
+  icon: {
+    color: '#fff',
+    fontWeight: 800,
+    fontSize: 24,
+  },
+  iconButton: {
+    marginLeft: 10,
+    padding: 15,
+    backgroundColor: theme.colors.lightPurple,
+    '&:hover': {
+      backgroundColor: theme.colors.navbarPurple,
+      opacity: 1,
+    }
+  },
+  noPairsContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  whiteIcon: {
+    color: '#fff',
+    paddingLeft: 5,
+  },
+  subtitle: {
+    color: '#fff',
+    marginTop: 16,
+  },
 }))
+
+const fetchPairs = async (id) => {
+  const response = await fetch(`/api/v1/collections/${id}/pairs`)
+  return await response.json()
+}
 
 const CollectionPairs = (props) => {
   const { collection } = props 
   const classes = useStyles()
 
+  // state
+  const [pairs, getPairs] = useState([])
+  const [isDialogOpen, openDialog] = useState(false)
+  const [pairData, getPairData] = useState({ language1: '', language2: '' })
+
+  useEffect(() => {
+    (async function() {
+      const incomingPairs = await fetchPairs(collection._id)
+      getPairs(incomingPairs.data)
+    })()
+  })
+
+  const savePairs = async () => {
+    const { language1, language2 } = pairData
+    const response = await fetch(`/api/v1/collections/${collection._id}/pairs`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ language1, language2 })
+    })
+    
+    const pairs = await response.json()
+    if (pairs.success) {
+      openDialog(false)
+      getPairData({ language1: '', language2: '' })
+    }
+  }
+
   return (
     <React.Fragment>
       <Typography variant='h4' className={classes.collectionName}>
         {collection.name}
+        <IconButton className={classes.iconButton} onClick={() => openDialog(true)}>
+          <PostAddIcon className={classes.icon} />
+        </IconButton>
       </Typography>
+      {
+        !pairs.length && (
+          <Typography className={classes.noPairsContainer}>
+            <span className={classes.subtitle}>You have no pairs. Connect them!</span>
+            <PeopleIcon className={classes.whiteIcon} />
+          </Typography>
+        )
+      }
+      {
+        Boolean(pairs.length) && (
+          pairs.map(pair => (
+            <div>
+              <span>{pair.language1}</span>
+              <span>{pair.language2}</span>
+            </div>
+          ))
+        )
+      }
+
+      <PairDialog 
+        open={isDialogOpen}
+        shouldDialogBeOpen={openDialog}
+        pairData={pairData}
+        getPairData={getPairData}
+        onSavePairs={savePairs}
+      />
     </React.Fragment>
   )
 
---
+++ client/src/components/dialogs/PairDialog.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const PairDialog = ({ 
+  open, shouldDialogBeOpen, getPairData, pairData,
+  onSavePairs,
+}) => (
+  <Dialog open={open} fullWidth maxWidth='md'>
+    <DialogTitle>Create a new pair</DialogTitle>
+    <DialogContent>
+      <TextField
+        label='Original'
+        placeholder='Enter your text here'
+        type='text'
+        fullWidth
+        required
+        // defaultValue={loginData.email}
+        onBlur={e => getPairData({ ...pairData, language1: e.target.value })}
+      />
+      <TextField
+        label='Translation'
+        placeholder='Enter your translation here'
+        type='text'
+        fullWidth
+        margin='normal'
+        required
+        // defaultValue={loginData.password}
+        onBlur={e => getPairData({ ...pairData, language2: e.target.value })}
+      />
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={() => { shouldDialogBeOpen(false) }} color='primary'>Cancel</Button>
+      <Button onClick={onSavePairs} color='primary'>Save</Button>
+    </DialogActions>
+  </Dialog>
+)
+
+export default PairDialog
