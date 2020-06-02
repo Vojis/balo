8fb994eabf96892a098e7032a995a30b1dccae17
voja <voja.milenkovic@nfq.asia>
2020-06-02 23:07:15 +0200

    Collapse or expand Panels

--- client/src/components/CollectionPairs.js
+++ client/src/components/CollectionPairs.js
@@ -5,6 +5,13 @@ import Typography from '@material-ui/core/Typography';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import PeopleIcon from '@material-ui/icons/People';
 import PairDialog from './dialogs/PairDialog';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
+import UnfoldLessIcon from '@material-ui/icons/UnfoldLess';
 
 const useStyles = makeStyles((theme) => ({
   collectionName: {
@@ -36,6 +43,10 @@ const useStyles = makeStyles((theme) => ({
     color: '#fff',
     marginTop: 16,
   },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
 }))
 
 const fetchPairs = async (id) => {
@@ -51,6 +62,8 @@ const CollectionPairs = (props) => {
   const [pairs, getPairs] = useState([])
   const [isDialogOpen, openDialog] = useState(false)
   const [pairData, getPairData] = useState({ language1: '', language2: '' })
+  const [expand, changeExpand] = useState({})
+  const [expandAll, changeExpandAll] = useState(false)
 
   useEffect(() => {
     (async function() {
@@ -58,6 +71,43 @@ const CollectionPairs = (props) => {
       getPairs(incomingPairs.data)
     })()
   })
+
+  const onExpandAll = () => {
+    let shouldExpand = false
+    const expandKeys = Object.keys(expand)
+    const valuesArray = expandKeys.map(key => expand[key])
+    
+    if (!valuesArray.length || valuesArray.indexOf(false) > -1) {
+      shouldExpand = true
+    }
+
+    const allIdsTrue = pairs.reduce((acc, item) => {
+      acc[item._id] = shouldExpand
+      return acc
+    }, {})
+
+    changeExpand(allIdsTrue)
+    changeExpandAll(shouldExpand)
+  }
+
+  const expandSingle = ({_id: id}) => {
+    let expandClickedItem = false
+
+    if (!expand[id]) {
+      expandClickedItem = true
+    }
+
+    const newState = { ...expand, [id]: expandClickedItem }
+    const expandKeys = Object.keys(newState)
+    const valuesArray = expandKeys.map(key => newState[key])
+
+    changeExpand(newState)
+    if (valuesArray.indexOf(false) === -1 && valuesArray.length === pairs.length) {
+      changeExpandAll(true)
+    } else {
+      changeExpandAll(false)
+    }
+  }
 
   const savePairs = async () => {
     const { language1, language2 } = pairData
@@ -76,12 +126,19 @@ const CollectionPairs = (props) => {
 
   return (
     <React.Fragment>
-      <Typography variant='h4' className={classes.collectionName}>
-        {collection.name}
-        <IconButton className={classes.iconButton} onClick={() => openDialog(true)}>
-          <PostAddIcon className={classes.icon} />
+      <div className={classes.buttonContainer}>
+        <Typography variant='h4' className={classes.collectionName}>
+          {collection.name}
+          <IconButton className={classes.iconButton} onClick={() => openDialog(true)}>
+            <PostAddIcon className={classes.icon} />
+          </IconButton>
+        </Typography>
+        <IconButton className={classes.iconButton} onClick={onExpandAll}>
+          {
+            expandAll ? <UnfoldLessIcon className={classes.icon} /> : <UnfoldMoreIcon className={classes.icon} /> 
+          }
         </IconButton>
-      </Typography>
+      </div>
       {
         !pairs.length && (
           <Typography className={classes.noPairsContainer}>
@@ -93,9 +150,15 @@ const CollectionPairs = (props) => {
       {
         Boolean(pairs.length) && (
           pairs.map(pair => (
-            <div>
-              <span>{pair.language1}</span>
-              <span>{pair.language2}</span>
+            <div style={{ marginTop: 16 }} key={pair._id}>
+              <ExpansionPanel expanded={expand[pair._id] || false} onClick={() => expandSingle(pair)}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.heading}>{pair.language2}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>{pair.language1}</Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
             </div>
           ))
         )
