29593db0ee6313220d8a215f7007c1a975942f70
voja <voja.milenkovic@nfq.asia>
2020-06-11 12:51:42 +0200

    Prepare server.js for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 require('colors');
@@ -28,6 +29,16 @@ app.use('/api/v1/pairs', pairRoutes);
 
 // Error Handler middleware
 app.use(errorHandler);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.cyan.inverse));
